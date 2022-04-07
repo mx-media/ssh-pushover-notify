e2f021d8c43eb326cea50f2b7bb9b7ab65abbb6d
mx-media <101652840+mx-media@users.noreply.github.com>
2022-04-07 19:06:52 +0200

    Update ssh-pushover.sh

--- ssh-pushover.sh
+++ ssh-pushover.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-hostname=$(hostname)
+HOSTNAME=$(hostname)
 PUSHOVER_USER="ux3hfgntjjmtaewo9wdgikg8iboc2m"
 PUSHOVER_API_TOKEN="ap2mj9v8w1wgkdo5nwyv7crusx233j"
 
 echo "================================="
-echo "Der SSH Zugriff auf $hostname wird jetzt überwacht."
+echo "Der SSH Zugriff auf $HOSTNAME wird jetzt überwacht."
 echo "Bei Zugriff bekommen Sie eine Nachricht auf Pushover."
 echo "================================="
 
@@ -14,5 +14,5 @@ tail -F /var/log/auth.log | gawk '{if(NR>10 && $0 ~ /sshd/ && $0 ~ /Accepted/)\
 -F \"token='$PUSHOVER_API_TOKEN'\" \
 -F \"user='$PUSHOVER_USER'\" \
 -F \"message=SSH Zugriff erfolgt durch %s von %s\" \
--F \"title='$hostname'\" https://api.pushover.net/1/messages.json",$9,$11); \
+-F \"title='$HOSTNAME'\" https://api.pushover.net/1/messages.json",$9,$11); \
 system(cmd)}}'
